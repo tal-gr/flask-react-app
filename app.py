75ab4282c2a9e100137d668a96796a0bc40f7f67
talgr <tal.gr@campus.technion.ac.il>
2022-02-05 21:28:22 +0200

    another small fix

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 api = Api(app)
 
 
-@app.route("/", defaults={'path':''})
+@app.route("/", defaults={'path': ''})
 def serve(path):
-    return send_from_directory(app.static_folder,'index.html')
+    return send_from_directory(app.static_folder, 'index.html')
 
 
 api.add_resource(HelloApiHandler, '/flask/hello')
